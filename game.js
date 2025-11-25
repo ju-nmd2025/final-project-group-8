4212f635c6525abdea7bcb3fd6a19faa70569133
rosenx <rosenxiong@gmail.com>
2025-11-25 10:10:20 +0100

    Update game.js

--- game.js
+++ game.js
@@ -55,5 +55,73 @@ class Paltform {
     this.borken = false;
     this.moveDir = 1;
 }
+  
+  update() {
+    // Moving platform
+    if (this.type === "moving") {
+      this.x += this.moveDir * 2;
+      if (this.x < 0 || this.x + this.w > width) {
+        this.moveDir *= -1;
+      }
+    }
+  }
+
+  draw() {
+    if (this.type === "normal") fill(50, 200, 50);
+    if (this.type === "moving") fill(50, 150, 250);
+    if (this.type === "broken") fill(200, 50, 50);
+
+    if (!this.broken) {
+      rect(this.x, this.y, this.w, this.h, 5);
+    }
+  }
+
+  // Player collision
+  hits(player) {
+    if (
+      player.vy > 0 && // only when falling down
+      player.x + player.w > this.x &&
+      player.x < this.x + this.w &&
+      player.y + player.h > this.y &&
+      player.y + player.h < this.y + this.h
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
+
+
+// ----- GLOBAL VARIABLES -----
+let player;
+let platforms = [];
+let scrollSpeed = 2; // platforms move down as player jumps
+
+
+// ----- SETUP -----
+function setup() {
+  createCanvas(400, 600);
+  player = new Player();
+  resetPlatforms();
+}
+
+
+// Create starting platforms
+function resetPlatforms() {
+  platforms = [];
+  for (let i = 0; i < 10; i++) {
+    let x = random(20, width - 100);
+    let y = i * 60;
+
+    // Random type
+    let r = random();
+    let type = "normal";
+
+    if (r < 0.1) type = "broken";
+    else if (r < 0.25) type = "moving";
+
+    platforms.push(new Platform(x, y, type));
+  }
 
   }
+
